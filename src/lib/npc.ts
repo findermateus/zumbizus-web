e556a200e42dd5db09d4bed050e7cc1012cec413
findermateus <mateusfinder@gmail.com>
2026-03-13 22:35:55 -0300

    chore: ajusta formatação, remove props não utilizadas e adiciona diretivas eslint em arquivos diversos

--- src/lib/npc.ts
+++ src/lib/npc.ts
@@ -1,8 +1,8 @@
-import {Session} from "next-auth";
+import { Session } from "next-auth";
 import NpcDAO from "@/lib/data-access-objects/npc-dao";
 
 export async function loadNpc(session: Session) {
-    const npcDAO = new NpcDAO();
+  const npcDAO = new NpcDAO();
 
-    return await npcDAO.findByUserId(session.user.userId);
+  return await npcDAO.findByUserId(session.user.userId);
 }
